739d6357cb53d646d351c92f70541a33c419ae00
David Surkov <surkovdavid@gmail.com>
2022-07-03 21:47:03 +0300

    add validators

--- src/dto/create-feedback.dto.ts
+++ src/dto/create-feedback.dto.ts
@@ -1,10 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateFeedbackDto {
   @IsString()
+  @Length(2, 100)
   readonly name: string;
   @IsString()
+  @IsEmail({}, { message: 'Email is not correct' })
   readonly email: string;
   @IsString()
+  @Length(2, 200)
   readonly message: string;
 }
